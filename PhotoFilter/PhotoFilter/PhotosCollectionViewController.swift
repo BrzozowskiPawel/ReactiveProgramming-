2ad1aec394530b7ed2c8a1001574a74d91bc252b
Paweł Brzozowski <mac@Pawes-MacBook-Air.local>
2022-01-22 19:28:12 +0100

    Access to the photo library

--- PhotoFilter/PhotoFilter/PhotosCollectionViewController.swift
+++ PhotoFilter/PhotoFilter/PhotosCollectionViewController.swift
@@ -7,7 +7,22 @@
 
 import Foundation
 import UIKit
+import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        populatePhotos()
+    }
+    
+    private func populatePhotos() {
+        // Request for user permission
+        PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+            if status == .authorized {
+                // Access to the photo lib
+            }
+        }
+    }
 }
