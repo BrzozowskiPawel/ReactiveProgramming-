916d22c22868f66eba0bc32b555ef2e1b4f9601a
Paweł Brzozowski <mac@Pawes-MacBook-Air.local>
2022-01-22 21:24:11 +0100

    using observabled and subscrition to pass photo

--- PhotoFilter/PhotoFilter/PhotosCollectionViewController.swift
+++ PhotoFilter/PhotoFilter/PhotosCollectionViewController.swift
@@ -8,8 +8,17 @@
 import Foundation
 import UIKit
 import Photos
+import RxSwift
 
 class PhotosCollectionViewController: UICollectionViewController {
+    
+    
+    // create a subject - can emit a vlaue and can be subscribed
+    private let selectedPhotoSubject = PublishSubject<UIImage>()
+    var selectedPhoto: Observable<UIImage> {
+        return selectedPhotoSubject.asObservable()
+    }
+    
     // Array fo asstes
     private var images = [PHAsset]()
     
@@ -19,6 +28,31 @@ class PhotosCollectionViewController: UICollectionViewController {
         
         // Populate with photos
         populatePhotos()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // Selected asset
+        let selectedAsset = self.images[indexPath.row]
+        
+        // Image from asset
+        PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFill, options: nil) { image, info in
+            guard let info = info else {
+                return
+            }
+            
+            // We dont want tumbnail image
+            let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
+            
+            // If it's not degraded take it
+            if !isDegradedImage {
+                if let image = image {
+                    // Publishing the subject
+                    self.selectedPhotoSubject.onNext(image)
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
--- PhotoFilter/PhotoFilter/ViewController.swift
+++ PhotoFilter/PhotoFilter/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageViewBG: UIImageView!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // Lager title
         self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navC = segue.destination as? UINavigationController, let photosCVC = navC.viewControllers.first as? PhotosCollectionViewController else {
+            fatalError("Segue destination not found")
+        }
+        
+        photosCVC.selectedPhoto.subscribe(onNext: { [weak self] (photo) in
+            self?.imageViewBG.image = photo
+        }).disposed(by: disposeBag)
     }
 
 
