d5c762d47ecdd0aeba68ce8a88ee602a0f4aed78
Paweł Brzozowski <mac@192.168.1.4>
2022-01-24 18:58:06 +0100

    created UI

---
+++ ToDoListRx/ToDoListRx/Controllers/TaskListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TaskListViewController.swift
+//  ToDoListRx
+//
+//  Created by Paweł Brzozowski on 24/01/2022.
+//
+
+import Foundation
+import UIKit
+
+class TaskListViewController: UIViewController {
+    
+    @IBOutlet weak var pioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
