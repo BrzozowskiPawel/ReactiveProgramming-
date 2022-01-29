87517a3baea6cd178ccb75c107f271ad22e03143
Paweł Brzozowski <mac@Pawes-MacBook-Air.local>
2022-01-29 19:02:50 +0100

    more about combine

--- CombinePlayground.playground/Contents.swift
+++ CombinePlayground.playground/Contents.swift
@@ -18,11 +18,34 @@ let subscriber = weatherPublisher.filter{$0 > 25}.sink { error in
 }
 
 
+// Handle different kind of events
+let anotherSubscriber = weatherPublisher.handleEvents { subscriptions in
+    print("New subscription \(subscriptions)")
+} receiveOutput: { output in
+    print("New value: Output \(output)")
+} receiveCompletion: { error in
+    print("Subscription completed with potential error \(error)")
+} receiveCancel: {
+    print("Subscriptionc canceled")
+} receiveRequest: { value in
+    print("Subscriber received value: \(value)")
+}.sink { error in
+    print("SINK error \(error)")
+} receiveValue: { value in
+    print("Subscriber received value: \(value) (sink)")
+}
+
+
+
 // Code for weather publisher to send temp values
 weatherPublisher.send(10)
 weatherPublisher.send(20)
 weatherPublisher.send(30)
 weatherPublisher.send(40)
+
+// Adding a error
+weatherPublisher.send(completion: Subscribers.Completion<WeatherError>.failure(.thingsJustHappen))
+
 weatherPublisher.send(50)
 
 
